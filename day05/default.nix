c079e6739e4844b88dac2aae990ea20516e14ecb
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-09 12:26:18 +0100

    wip day05 part2

--- day05/default.nix
+++ day05/default.nix
@@ -1,22 +1,23 @@
 {pkgs ? import ../locked.nix}: let
     lib = pkgs.lib;
 
-    inherit (builtins) filter length genList elemAt map head tail groupBy attrValues readFile all elem foldl';
+    inherit (builtins) filter length genList elemAt map head tail groupBy attrValues readFile all elem foldl' listToAttrs;
     inherit (lib.strings) trim splitString toInt;
-    inherit (lib.lists) imap0 last flatten take zipListsWith sublist;
+    inherit (lib.lists) imap0 last flatten take zipListsWith sublist sort;
     inherit (lib) filterAttrs;
+
+    isCorrect = rules: first: rest: rules
+        |> filter (rule: elemAt rule 0 == first)
+        |> map (rule: elemAt rule 1)
+        |> (r: all (item: elem item r) rest)
+    ;
 
     isCorrectOrder = rules: list: let
         recurse = rules: list: let
             first = head list;
             rest = tail list;
 
-            firstIsCorrect = rules
-                |> filter (rule: elemAt rule 0 == first)
-                |> map (rule: elemAt rule 1)
-                |> (ruleItems: rest
-                    |> all (item: elem item ruleItems)
-                );
+            firstIsCorrect = isCorrect rules first rest;
         in
             if rest == [] then firstIsCorrect
             else if firstIsCorrect then recurse rules rest
@@ -33,7 +34,7 @@
         |> (l: elemAt l middle)
     ;
 
-    part0 = text: text
+    mkEvaluatedList = useRules: text: text
         |> trim
         |> splitString "\n\n"
         |> (input: let
@@ -49,16 +50,54 @@
             pageNumbers
             |> map (isCorrectOrder rules)
             |> zipListsWith (list: correct: {inherit list correct;}) pageNumbers
-            |> filter (x: x.correct)
-            |> map (x: x.list)
-            # |> (l: lib.debug.traceSeq {inherit l;} l)
-            |> map getMiddle
-            |> map toInt
-            |> foldl' (a: b: a + b) 0
-        )
+            |> useRules rules
+        );
+
+    part0 = text: text
+        |> mkEvaluatedList (rules: list: list)
+        |> filter (x: x.correct)
+        |> map (x: x.list)
+        |> map getMiddle
+        |> map toInt
+        |> foldl' (a: b: a + b) 0
     ;
 
-    part1 = text: "TODO P2";
+    sortByRules = rules: list: let
+        recurse = rules: list: let
+            firstIsCorrect = isCorrect rules (head list) (tail list);
+            swap = indexA: indexB: list: list
+                |> length
+                |> genList (i:
+                    if i == indexA then elemAt list indexB
+                    else if i == indexB then elemAt list indexA
+                    else elemAt list i
+                );
+        in
+            lib.debug.traceSeq {inherit firstIsCorrect list;}
+            (
+            if firstIsCorrect then
+                list
+            else
+                swap 0 1 list
+        )
+        ;
+    in
+        lib.debug.traceSeq {inherit list;} (
+        recurse rules list
+            )
+    ;
+
+    part1 = text: mkEvaluatedList
+        (rules: list: list
+            |> filter (x: !x.correct)
+            |> map (x: x.list)
+            |> take 1
+            |> map (sortByRules rules)
+            # |> map (map toInt)
+            # |> map (sort (a: b: a > b))
+            # |> map (isCorrectOrder rules)
+        ) text
+    ;
 
     solve = filePath: let
         text = readFile filePath;
